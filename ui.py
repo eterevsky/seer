d304c0fc52f8f8b004df4eccb5cfdb3a4104214e
Oleg Eterevsky <oleg@eterevsky.com>
2020-05-26 21:24:33 +0200

    Manage focus between several TextInput's.
    Also make it possible to first create StackLayout's and only later add their children.

--- ui.py
+++ ui.py
@@ -1,5 +1,7 @@
 import enum
+import functools
 import pyglet
+from pyglet.window import key
 from typing import List, Union
 
 
@@ -14,10 +16,10 @@ class Pane(pyglet.event.EventDispatcher):
                  background=None):
         self.content_width = content_width
         self.content_height = content_height
-        self.offset_x = None
-        self.offset_y = None
-        self.width = None
-        self.height = None
+        self.offset_x = 0
+        self.offset_y = 0
+        self.width = 0
+        self.height = 0
         self.background = background
 
     @property
@@ -73,6 +75,17 @@ Pane.register_event_type('on_mouse_motion')
 Pane.register_event_type('on_mouse_release')
 Pane.register_event_type('on_mouse_scroll')
 Pane.register_event_type('on_resize')
+Pane.register_event_type('on_content_resize')
+
+
+class Controller(object):
+    def __init__(self,
+            pane: Union[pyglet.window.Window, Pane] = None, **kwargs):
+        if pane is None:
+            self.pane = Pane(**kwargs)
+        else:
+            self.pane = pane
+        self.pane.content = self
 
 
 class Orientation(enum.Enum):
@@ -80,30 +93,28 @@ class Orientation(enum.Enum):
     VERTICAL = 2
 
 
-class StackLayout(object):
+class StackLayout(Controller):
     def __init__(self,
                  orientation: Orientation,
-                 parent: Union[pyglet.window.Window, Pane],
-                 children: List[Pane]):
+                 parent: Union[pyglet.window.Window, Pane] = None,
+                 **kwargs):
+        super().__init__(parent, **kwargs)
         self.orientation = orientation
-        self.parent = parent
-        self.children = children
-        self._resize()
-        self.parent.push_handlers(self)
+        self.children = []
+        self.pane.push_handlers(self)
         self.mouseover_child = None
         self._dragging_child = None
         self._dragging_button = 0
 
     @property
     def offset_x(self):
-        return getattr(self.parent, 'offset_x', 0)
+        return getattr(self.pane, 'offset_x', 0)
 
     @property
     def offset_y(self):
-        return getattr(self.parent, 'offset_y', 0)
+        return getattr(self.pane, 'offset_y', 0)
 
     def on_draw(self):
-
         for child in self.children:
             child.dispatch_event('on_draw')
 
@@ -158,13 +169,20 @@ class StackLayout(object):
     def on_resize(self, width, height, offset_x=0, offset_y=0):
         self._resize()
 
+    def add_child(self, child: Union[Pane, Controller]) -> None:
+        if isinstance(child, Pane):
+            self.children.append(child)
+        else:
+            self.children.append(child.pane)
+        self._resize()
+
     def _resize(self):
         if self.orientation == Orientation.HORIZONTAL:
-            dim = self.parent.width
+            dim = self.pane.width
             content_dims = [child.content_width for child in self.children]
             offset = self.offset_x
         elif self.orientation == Orientation.VERTICAL:
-            dim = self.parent.height
+            dim = self.pane.height
             content_dims = [child.content_height for child in self.children]
             offset = self.offset_y + dim
         else:
@@ -181,12 +199,12 @@ class StackLayout(object):
                 d = dim
             if self.orientation == Orientation.HORIZONTAL:
                 child.dispatch_event(
-                    'on_resize', d, self.parent.height, offset, self.offset_y)
+                    'on_resize', d, self.pane.height, offset, self.offset_y)
                 offset += d
             else:
                 offset -= d
                 child.dispatch_event(
-                    'on_resize', self.parent.width, d, self.offset_x, offset)
+                    'on_resize', self.pane.width, d, self.offset_x, offset)
 
     def _find_child(self, x, y):
         """Returns the child contining (x, y) or None."""
@@ -199,9 +217,10 @@ class StackLayout(object):
         return None
 
 
-class TextInput(object):
+class TextInput(Controller):
     def __init__(self, content_width=100, content_height=100, background=None):
-        self.pane = Pane(content_width=content_width, content_height=content_height,
+        super().__init__(content_width=content_width,
+                         content_height=content_height,
                          background=background)
         self.pane.push_handlers(self)
         self.pane.content = self
@@ -210,9 +229,14 @@ class TextInput(object):
             self.document, content_width, content_height, multiline=True,
             wrap_lines=True)
         self.caret = pyglet.text.caret.Caret(self.layout)
-        self.caret.visible = True
+        self.caret.visible = False
 
     def on_resize(self, width, height, offset_x, offset_y):
+        print('on_resize', width, height, offset_x, offset_y)
+        if width <= 0 or height <= 0:
+            self.caret.visible = False
+            return
+        self.caret.visible = True
         self.layout.width = width
         self.layout.height = height
         self.layout.x = offset_x
@@ -221,5 +245,74 @@ class TextInput(object):
     def on_draw(self):
         self.layout.draw()
 
+    def on_return(self):
+        pass
+
+
+class FocusManager(object):
+    def __init__(self, window):
+        self.window = window
+        window.push_handlers(self)
+        self._inputs = []
+        self._focus = None
+        self._text_cursor = window.get_system_mouse_cursor('text')
+        print(self._text_cursor)
+
+    def add_input(self, controller):
+        self._inputs.append(controller)
+
+    def _find_input(self, x, y):
+        if self._focus is not None and self._focus.pane.contains(x, y):
+            return self._focus
+        for controller in self._inputs:
+            if controller.pane.contains(x, y):
+                return controller
+        return None
+
     def on_mouse_press(self, x, y, button, modifiers):
-        self.caret.on_mouse_press(x, y, button, modifiers)
+        target = self._find_input(x, y)
+        if target is not self._focus and self._focus is not None:
+            print('switch')
+            self._focus.caret.visible = False
+        self._focus = target
+        if target is not None:
+            self._focus.caret.visible = True
+            return target.caret.on_mouse_press(x, y, button, modifiers)
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        target = self._find_input(x, y)
+        print('on_mouse_motion', target)
+        self.window.set_mouse_cursor(
+            None if target is None else self._text_cursor)
+
+    def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
+        if self._focus:
+            return self._focus.caret.on_mouse_drag(x, y, dx, dy, buttons, modifiers)
+
+    def on_text(self, text):
+        if self._focus:
+            return self._focus.caret.on_text(text)
+
+    def on_text_motion(self, motion):
+        if self._focus:
+            return self._focus.caret.on_text_motion(motion)
+
+    def on_text_motion_select(self, motion):
+        if self._focus:
+            return self._focus.caret.on_text_motion_select(motion)
+
+    def on_key_press(self, symbol, modifiers):
+        if not self._focus:
+            return pyglet.event.EVENT_UNHANDLED
+        if symbol == key.ESCAPE:
+            self._focus = None
+            self._focus.caret.visible = False
+            return pyglet.event.EVENT_HANDLED
+        if symbol == key.ENTER:
+            return self._focus.on_return()
+
+        if key.SPACE <= symbol <= key.ASCIITILDE or symbol in (
+            key.UP, key.RIGHT, key.DOWN, key.LEFT,
+            key.BACKSPACE, key.DELETE):
+            return pyglet.event.EVENT_HANDLED
+        return pyglet.event.EVENT_UNHANDLED
--- ui_test.py
+++ ui_test.py
@@ -8,32 +8,20 @@ window = pyglet.window.Window(resizable=True)
 def update(dt): pass
 pyglet.clock.schedule_interval(update, 0.001)
 
-pane1 = ui.Pane(content_width=200, background=(0, 255, 0))
-pane2 = ui.Pane(background=(0, 0, 255))
-pane3 = ui.Pane(content_width=100, background=(255, 0, 0))
-layout = ui.StackLayout(ui.Orientation.HORIZONTAL, window, (pane1, pane2, pane3))
-pane10 = ui.Pane(background=(64, 64, 64))
-text = ui.TextInput(content_width=200, background=(128, 128, 128))
-# pane11 = ui.Pane(content_height=200, background=(128, 128, 128))
-column1 = ui.StackLayout(ui.Orientation.VERTICAL, pane1, (pane10, text.pane))
+focus_manager = ui.FocusManager(window)
 
-window.push_handlers(
-    on_text=text.caret.on_text,
-    on_text_motion=text.caret.on_text_motion,
-    on_text_motion_select=text.caret.on_text_motion_select)
-
-
-def on_mouse_press1(x, y, button, modifiers):
-    print('on_mouse_press1', x, y, button, modifiers)
-pane1.push_handlers(on_mouse_press=on_mouse_press1)
-
-def on_mouse_press2(x, y, button, modifiers):
-    print('on_mouse_press2', x, y, button, modifiers)
-pane2.push_handlers(on_mouse_press=on_mouse_press2)
-
-def on_mouse_press3(x, y, button, modifiers):
-    print('on_mouse_press3', x, y, button, modifiers)
-pane3.push_handlers(on_mouse_press=on_mouse_press3)
-
+layout = ui.StackLayout(ui.Orientation.HORIZONTAL, window)
+column1 = ui.StackLayout(
+    ui.Orientation.VERTICAL, content_width=200, background=(0, 255, 0))
+column1.add_child(ui.Pane(background=(64, 64, 64)))
+text1 = ui.TextInput(content_height=200, background=(128, 128, 128))
+focus_manager.add_input(text1)
+column1.add_child(text1)
+text2 = ui.TextInput(content_height=100, background=(160, 160, 160))
+focus_manager.add_input(text2)
+column1.add_child(text2)
+layout.add_child(column1)
+layout.add_child(ui.Pane(background=(0, 0, 255)))
+layout.add_child(ui.Pane(content_width=100, background=(255, 0, 0)))
 
 pyglet.app.run()
